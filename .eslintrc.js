8f403ace10dd70113d702c9bc58755df912a514f
TortuC <tortuc611@gmail.com>
2021-06-15 04:13:41 +0800

    version updage

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,13 @@
 // .eslintrc.js
 module.exports = {
+  ignorePatterns: [".eslintrc.js"],
   extends: ['airbnb-typescript-prettier'],
   rules: {
     'no-param-reassign': 0,
     '@typescript-eslint/no-use-before-define': 0,
     'no-console': 0,
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off"
   },
   env: {
     'jest': true,
